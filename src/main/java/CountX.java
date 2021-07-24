3656a14a4a3edf8f9b6937ad59d8fc43ec1be23a
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-24 17:38:24 +0530

    Count the no of 'x' in a string

--- src/main/java/CountX.java
+++ src/main/java/CountX.java
@@ -1,27 +1,26 @@
 public class CountX {
     /**
      * Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
-     * countX("xxhixx") → 4
+
+     * countX() → 4
      * countX("xhixhix") → 3
      * countX("hi") → 0
      */
-    static int i;
-    public static int countX(String str){
-        int count = 0;
-        int len = str.length();
-        while(i<len) {
-            if (str.charAt(i) == 'x') {
-                count++;
-                i++;
-                return count +countX(str);
-            }
+
+    public static int countX(String str,int i,int count){
+        if(str.length()==i) return count;
+        if(str.charAt(i)=='x') {
             i++;
-            return count+countX(str);
+            count++;
+            return countX(str,i,count);
         }
-        return count;
+        i++;
+        return countX(str,i,count);
+
     }
 
     public static void main(String[] args) {
-        System.out.println(countX("bhqnu"));
+        System.out.println( countX("xxxxaaxx",0,0));
     }
+
 }
