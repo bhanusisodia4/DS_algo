6482f35af7df87ba30560b9e118c27cab195af5e
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-07-30 22:29:52 +0530

    Linked list more functions

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -2,13 +2,53 @@ public class Node {
     int data;
     Node next;
 
-    private static void print(Node temp){
+    private static void print(Node head){
+        Node temp = new Node();
+        temp=head;
         while(temp!=null) {
-            System.out.println(temp.data);
+            System.out.print(" "+temp.data);
             temp= temp.next;
         }
     }
+    private static Node insertAtStart(Node head,int data){
+        Node temp = new Node();
+        temp.data=data;
+        temp.next= head;
+        return  temp;
+    }
 
+    private static Node insertAtLast(Node head, int data) {
+        Node temp = new Node();
+        Node temp2 = new Node();
+        temp2 = head;
+        while (temp2.next != null) {
+            temp2 = temp2.next;
+        }
+        temp2.next = temp;
+        temp.data = data;
+        return head;
+    }
+    private static void middleIndex(Node head){
+        Node slow = new Node();
+        Node fast = new Node();
+        Node prev = new Node();
+
+        slow=head;
+        fast=head;
+
+        while(fast!=null&& fast.next!=null){
+            prev=slow;
+            slow= slow.next;
+            fast= fast.next.next;
+        }
+        if(fast==null){
+            System.out.println("linked list is even: "+prev.data+" "+slow.data);
+        }
+        else{
+            System.out.println("Linked list is odd: "+slow.data);
+        }
+
+    }
     public static void main(String[] args) {
         Node first = new Node();
         first.data=1;
@@ -27,5 +67,15 @@ public class Node {
         second.next= third;
 
         print(first);
+        System.out.println();
+       Node head= insertAtStart(first,44);
+        print(head);
+        System.out.println();
+       head= insertAtLast(head,33);
+       print(head);
+        System.out.println();
+
+        middleIndex(head);
+
     }
 }
